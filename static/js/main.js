fb41b16cf457b648268370aeaf2b2675e1a13cf9
MolefeRamotsepane <ramotsepanem@gmail.com>
2026-09-20 18:55:37 +0200

    Add account access and main page templates with styling enhancements

---
+++ static/js/main.js
@@ -0,0 +1,7 @@
+// Section apperance and navigation activeness (using active class)
+
+const home = document.querySelector('#home');
+const chat = document.querySelector('#chat');
+const alert = document.querySelector('#alert');
+const profile = document.querySelector('#profile');
+
